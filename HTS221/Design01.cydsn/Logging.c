96f600911875674f8624f12978558c4e7b86a88a
Davide Marzorati B3 <davide.marzorati@polimi.it>
2019-09-24 17:34:40 +0200

    Implemented some functions for the logging interface

--- HTS221/Design01.cydsn/Logging.c
+++ HTS221/Design01.cydsn/Logging.c
@@ -45,8 +45,55 @@ LoggingError Logging_PrintHTS221Configuration(HTS221_Struct* hts221)
 {
     char str[50] = {'\0'};
     Logging_Interface_PutString("HTS221 Configuration\n");
-    Logging_Interface_PutString("\t-HTS221_AVGH:");
+    // Print out average humidity samples
+    Logging_Interface_PutString("\t- HTS221_AVGH:");
     sprintf(str,"%d\n", hts221->avgHum);
+    Logging_Interface_PutString(str);
+    // Print out average temperature samples
+    Logging_Interface_PutString("\t- HTS221_AVGT:");
+    sprintf(str,"%d\n", hts221->avgTemp);
+    Logging_Interface_PutString(str);
+    // Heater status
+    Logging_Interface_PutString("\t- HTS221_Heater:");
+    if (hts221->heater == HTS221_HEATER_OFF)
+    {
+        Logging_Interface_PutString("OFF\n");
+    }
+    else
+    {
+        Logging_Interface_PutString("ON\n");
+    }
+    // Power status
+    Logging_Interface_PutString("\t- HTS221_Power:");
+    if (hts221->power == HTS221_POWER_OFF)
+    {
+        Logging_Interface_PutString("OFF\n");
+    }
+    else
+    {
+        Logging_Interface_PutString("ON\n");
+    }
+    // Output data rate
+    Logging_Interface_PutString("\t- HTS221_ODR:");
+    switch(hts221->odr)
+    {
+        case HTS221_ODR_OneShot:
+            Logging_Interface_PutString("One Shot\n");
+        break;
+        case HTS221_ODR_1Hz:
+            Logging_Interface_PutString("1 Hz\n");
+        break;
+        case HTS221_ODR_7Hz:
+            Logging_Interface_PutString("7 Hz\n");
+        break;
+        case HTS221_ODR_12_5Hz:
+            Logging_Interface_PutString("12.5 Hz\n");
+        break;
+        default:
+            Logging_Interface_PutString("Unknown\n");
+        break;
+    }
+    
     return LOGGING_OK;
 }
 
